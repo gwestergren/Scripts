48c4423dbaba56e46e1eb75baeb53474d42deff8
gwestergren <gwestergren@wtandc.com>
2022-02-03 10:59:10 -0800

    Update DeviceSearch.ps1

--- DeviceSearch.ps1
+++ DeviceSearch.ps1
@@ -1,5 +1,5 @@
 ﻿$pcname= Read-Host -Prompt "Enter system name"
-$files = Get-ChildItem -path filesystem::'\\llb-pkg01\source\Remediation source\Reports\sccmdevicelist*.csv' | Select -ExpandProperty FullName
+$files = Get-ChildItem -path filesystem::'\\llb-pkg01\source\Remediation source\Reports\sccmdevicelist*.csv' | Select-Object -ExpandProperty FullName
 
 $userlist = foreach ($file in $files){
 $filename = ($file.Split("\")[-1]).split(".")[0]
@@ -9,4 +9,4 @@ $filetime = (($filename.Split("\")[-1]).split("_"))[2]
 Import-CSV -Path filesystem::$file | Select-Object *,@{Name='filedate';Expression={$filedate}},@{Name='filetime';Expression={$filetime}}
                                     }
 
-$userlist | Where-Object {$_.name -eq $pcname} | ft -AutoSize name, LastActiveTime, IsActive, CNIsOnline, UserName, User, PingStatus, FileDate, FileTime
+$userlist | Where-Object {$_.name -eq $pcname} | Format-Table -AutoSize name, LastActiveTime, IsActive, CNIsOnline, UserName, User, PingStatus, FileDate, FileTime
